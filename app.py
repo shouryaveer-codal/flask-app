870467fefb1829d649c835487302276b8e0ee04e
Shouryaveer Mehta <smehta@codal.com>
2023-02-18 19:10:51 +0530

    Add api.py and requirements.txt file

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    try:
+        response = {}
+        response["status_code"] = 200
+        response["message"] = "API deployed on AWS successfully"
+    except Exception as e:
+        print(e)
+        response["error"] = "Something went wrong."
+        response["status_code"] = "500"
+    return app.response_class(
+            response=json.dumps(response), status=response["status_code"],
+            mimetype="application/json"
+        )
